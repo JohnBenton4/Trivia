9d5f96664c9f061cdc2ef69dbdcab24e52a51c15
John Benton <jtbentoniv@gmail.com>
2022-11-30 14:37:57 -0500

    made the app prettier

--- sad.js
+++ sad.js
@@ -2,21 +2,15 @@ const jokeButton = document.querySelector('.btn.btn-warning.btn');
 const jokeHolder = document.querySelector('.joke');
 
 
-// fetch('https://api.humorapi.com/jokes/search')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-
 async function fetchJoke() {
-    const response = await fetch ("https://icanhazdadjoke.com", {
+    const response = await fetch("https://icanhazdadjoke.com", {
         headers: {
             Accept: "application/json",
 
         },
     });
-    // console.log(response);
     const data = response.json();
     return data;
-
 }
 
 async function handleClick() {
